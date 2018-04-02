1056e2b992168a676f0130f29fe511afd2644a91
Johannes Siipola <johannes.siipola@creuna.fi>
2018-04-02 21:47:51 +0300

    Add admin UI

--- searchwp-finnish-base-forms.php
+++ searchwp-finnish-base-forms.php
@@ -3,7 +3,7 @@
 Plugin Name: SearchWP Finnish Base Forms
 Plugin URI: https://github.com/joppuyo/searchwp-finnish-base-forms
 Description: SearchWP plugin to add Finnish base forms in search index
-Version: 0.0.0
+Version: 1.0.0
 Author: Johannes Siipola
 Author URI: https://siipo.la
 Text Domain: searchwp-finnish-base-forms
@@ -13,39 +13,96 @@ defined('ABSPATH') or die('I wish I was using a real MVC framework');
 
 require 'vendor/autoload.php';
 
-add_filter('searchwp_indexer_pre_process_content', function ($content) {
-    $tokenizer = new \NlpTools\Tokenizers\WhitespaceAndPunctuationTokenizer();
-    $tokenized = $tokenizer->tokenize(strip_tags($content));
+if (get_option('searchwp_finnish_base_forms_api_url')) {
+    add_filter('searchwp_indexer_pre_process_content', function ($content) {
+        $tokenizer = new \NlpTools\Tokenizers\WhitespaceAndPunctuationTokenizer();
+        $tokenized = $tokenizer->tokenize(strip_tags($content));
 
-    $apiRoot = 'http://xxx';
+        $apiRoot = get_option('searchwp_finnish_base_forms_api_url');
 
-    $client = new \GuzzleHttp\Client();
+        $client = new \GuzzleHttp\Client();
 
-    $extraWords = [];
+        $extraWords = [];
 
-    $requests = function () use ($client, $tokenized, $apiRoot) {
-        foreach ($tokenized as $token) {
-            yield function () use ($client, $token, $apiRoot) {
-                return $client->getAsync($apiRoot . '/analyze/' . $token);
-            };
-        }
-    };
-
-    $pool = new \GuzzleHttp\Pool($client, $requests(), [
-        'concurrency' => 10,
-        'fulfilled' => function ($response) use (&$extraWords) {
-            $response = json_decode($response->getBody()->getContents(), true);
-            if (count($response)) {
-                array_push($extraWords, $response[0]['BASEFORM']);
+        $requests = function () use ($client, $tokenized, $apiRoot) {
+            foreach ($tokenized as $token) {
+                yield function () use ($client, $token, $apiRoot) {
+                    return $client->getAsync($apiRoot . '/analyze/' . $token);
+                };
             }
-        },
-    ]);
+        };
 
-    $promise = $pool->promise();
-    $promise->wait();
+        $pool = new \GuzzleHttp\Pool($client, $requests(), [
+            'concurrency' => 10,
+            'fulfilled' => function ($response) use (&$extraWords) {
+                $response = json_decode($response->getBody()->getContents(), true);
+                if (count($response)) {
+                    array_push($extraWords, $response[0]['BASEFORM']);
+                }
+            },
+        ]);
 
-    $content = $content . ' ' . implode(' ', $extraWords);
+        $promise = $pool->promise();
+        $promise->wait();
 
-    return $content;
+        $content = $content . ' ' . implode(' ', $extraWords);
+
+        return $content;
+    });
+}
+
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), function ($links) {
+    $settings_link = '<a href="options-general.php?page=searchwp_finnish_base_forms">' . __('Settings', 'searchwp_finnish_base_forms') . '</a>';
+    array_push($links, $settings_link);
+    return $links;
 });
 
+function searchwp_finnish_base_forms_settings_page()
+{
+    $updated = false;
+    if (!empty($_POST['submit'])) {
+        check_admin_referer('searchwp_finnish_base_forms');
+        update_option('searchwp_finnish_base_forms_api_url', $_POST['api_url']);
+        $updated = true;
+    }
+
+    $apiUrl = get_option('searchwp_finnish_base_forms_api_url');
+
+    echo '<div class="wrap">';
+    echo '    <h1>' . __('SearchWP Finnish Base Forms', 'searchwp_finnish_base_forms') . '</h1>';
+    if ($updated) {
+        echo '<div class="notice notice-success">';
+        echo '    <p>' . __('Node API URL Updated', 'searchwp_finnish_base_forms') . '</p>';
+        echo '</div>';
+    }
+    echo '    <form method="post">';
+    echo '    <table class="form-table">';
+    echo '        <tbody>';
+    echo '            <tr>';
+    echo '                <th scope="row">';
+    echo '                    <label for="api_url">' . __('Node API URL', 'searchwp_finnish_base_forms') . '</label>';
+    echo '                </th>';
+    echo '                <td>';
+    echo '                <input name="api_url" type="url" id="api_url" value="' . esc_url($apiUrl) . '" class="regular-text">';
+    echo '                </td>';
+    echo '            </tr>';
+    echo '        </tbody>';
+    echo '    </table>';
+    echo '    <p class="submit">';
+    echo '        <input class="button-primary" type="submit" name="submit" value="Save">';
+    echo '    </p>';
+    wp_nonce_field('searchwp_finnish_base_forms');
+    echo '    </form>';
+    echo '</div>';
+}
+
+add_action('admin_menu', function () {
+    add_submenu_page(
+        null,
+        __('SearchWP Finnish Base Forms', 'searchwp_finnish_base_forms'),
+        __('SearchWP Finnish Base Forms', 'searchwp_finnish_base_forms'),
+        'manage_options',
+        'searchwp_finnish_base_forms',
+        'searchwp_finnish_base_forms_settings_page'
+    );
+});
