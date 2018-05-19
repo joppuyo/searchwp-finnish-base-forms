f4aa4e23b43ca930775b7a56795f10682eff34bd
Johannes Siipola <johannes@siipo.la>
2018-05-19 14:01:28 +0300

    Add option to also lemmatize the search query

--- searchwp-finnish-base-forms.php
+++ searchwp-finnish-base-forms.php
@@ -18,40 +18,17 @@ if (file_exists(__DIR__ . '/vendor')) {
 
 if (get_option('searchwp_finnish_base_forms_api_url')) {
     add_filter('searchwp_indexer_pre_process_content', function ($content) {
-        $tokenizer = new \NlpTools\Tokenizers\WhitespaceAndPunctuationTokenizer();
-        $tokenized = $tokenizer->tokenize(strip_tags($content));
-
-        $apiRoot = get_option('searchwp_finnish_base_forms_api_url');
-
-        $client = new \GuzzleHttp\Client();
-
-        $extraWords = [];
-
-        $requests = function () use ($client, $tokenized, $apiRoot) {
-            foreach ($tokenized as $token) {
-                yield function () use ($client, $token, $apiRoot) {
-                    return $client->getAsync(trailingslashit($apiRoot) . 'analyze/' . $token);
-                };
-            }
-        };
-
-        $pool = new \GuzzleHttp\Pool($client, $requests(), [
-            'concurrency' => 10,
-            'fulfilled' => function ($response) use (&$extraWords) {
-                $response = json_decode($response->getBody()->getContents(), true);
-                if (count($response)) {
-                    array_push($extraWords, $response[0]['BASEFORM']);
-                }
-            },
-        ]);
-
-        $promise = $pool->promise();
-        $promise->wait();
-
-        $content = $content . ' ' . implode(' ', $extraWords);
-
-        return $content;
+        return searchwp_finnish_base_forms_lemmatize($content);
     });
+}
+
+if (get_option('searchwp_finnish_base_forms_api_url') && get_option('searchwp_finnish_base_forms_lemmatize_search_query')) {
+    add_filter('searchwp_pre_search_terms', function ($terms, $engine) {
+        $terms = implode(' ', $terms);
+        $terms = searchwp_finnish_base_forms_lemmatize($terms);
+        $terms = explode(' ', $terms);
+        return $terms;
+    }, 10, 2);
 }
 
 add_filter('plugin_action_links_' . plugin_basename(__FILE__), function ($links) {
@@ -66,6 +43,7 @@ function searchwp_finnish_base_forms_settings_page()
     if (!empty($_POST['submit'])) {
         check_admin_referer('searchwp_finnish_base_forms');
         update_option('searchwp_finnish_base_forms_api_url', $_POST['api_url']);
+        update_option('searchwp_finnish_base_forms_lemmatize_search_query', $_POST['lemmatize_search_query'] ? 1 : 0);
         $updated = true;
     }
 
@@ -75,7 +53,7 @@ function searchwp_finnish_base_forms_settings_page()
     echo '    <h1>' . __('SearchWP Finnish Base Forms', 'searchwp_finnish_base_forms') . '</h1>';
     if ($updated) {
         echo '<div class="notice notice-success">';
-        echo '    <p>' . __('Node API URL Updated', 'searchwp_finnish_base_forms') . '</p>';
+        echo '    <p>' . __('Options have been updated', 'searchwp_finnish_base_forms') . '</p>';
         echo '</div>';
     }
     echo '    <form method="post">';
@@ -89,6 +67,20 @@ function searchwp_finnish_base_forms_settings_page()
     echo '                <input name="api_url" type="url" id="api_url" value="' . esc_url($apiUrl) . '" class="regular-text">';
     echo '                </td>';
     echo '            </tr>';
+    echo '            <tr>';
+    echo '                <th scope="row">';
+    echo '                    <label>' . __('Add base forms to search query', 'searchwp_finnish_base_forms') . '</label>';
+    echo '                </th>';
+    echo '                <td>';
+    echo '                <input type="checkbox" name="lemmatize_search_query" id="lemmatize_search_query" value="1" ' . checked(get_option('searchwp_finnish_base_forms_lemmatize_search_query') === '1', true, false) . ' />';
+    echo '                <label for="lemmatize_search_query">Enabled</label>';
+    echo '                </td>';
+    echo '            </tr>';
+    echo '            <tr>';
+    echo '                <th colspan="2">';
+    echo '                <span style="font-weight: 400">Note: if you enable "Add base forms to search query", the API will be called every time a search if performed, this might have performance implications.</span>';
+    echo '                </td>';
+    echo '            </tr>';
     echo '        </tbody>';
     echo '    </table>';
     echo '    <p class="submit">';
@@ -97,6 +89,42 @@ function searchwp_finnish_base_forms_settings_page()
     wp_nonce_field('searchwp_finnish_base_forms');
     echo '    </form>';
     echo '</div>';
+}
+
+function searchwp_finnish_base_forms_lemmatize($content) {
+    $tokenizer = new \NlpTools\Tokenizers\WhitespaceAndPunctuationTokenizer();
+    $tokenized = $tokenizer->tokenize(strip_tags($content));
+
+    $apiRoot = get_option('searchwp_finnish_base_forms_api_url');
+
+    $client = new \GuzzleHttp\Client();
+
+    $extraWords = [];
+
+    $requests = function () use ($client, $tokenized, $apiRoot) {
+        foreach ($tokenized as $token) {
+            yield function () use ($client, $token, $apiRoot) {
+                return $client->getAsync(trailingslashit($apiRoot) . 'analyze/' . $token);
+            };
+        }
+    };
+
+    $pool = new \GuzzleHttp\Pool($client, $requests(), [
+      'concurrency' => 10,
+      'fulfilled' => function ($response) use (&$extraWords) {
+          $response = json_decode($response->getBody()->getContents(), true);
+          if (count($response)) {
+              array_push($extraWords, $response[0]['BASEFORM']);
+          }
+      },
+    ]);
+
+    $promise = $pool->promise();
+    $promise->wait();
+
+    $content = $content . ' ' . implode(' ', $extraWords);
+
+    return $content;
 }
 
 add_action('admin_menu', function () {
